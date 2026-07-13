37429e3204e7d07a900362b5c7534f762d6057a3
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-07-13 11:30:56 +0200

    fix(nix): pin rust overlay commit

--- module/rust-overlay.nix
+++ module/rust-overlay.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  url = "https://github.com/oxalica/rust-overlay/archive/e3fa5cf86b93914b8f312b2a1ca14fbb139c655c.tar.gz";
   sha256 = "sha256:04ci7amdr6xxdd802dv0i2csvwlqyj603w9kv40r7jn343vqmihy";
 })
