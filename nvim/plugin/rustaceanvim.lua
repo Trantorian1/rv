f8578174a525caa0d3d2c00f6b5d90ae9d181065
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-09-10 11:10:08 +0200

    feat(rust): rust-specific commands

--- nvim/plugin/rustaceanvim.lua
+++ nvim/plugin/rustaceanvim.lua
@@ -3,14 +3,60 @@ if vim.g.did_load_rustaceanvim then
 end
 vim.g.did_load_rustaceanvim = true
 
+---@type rustaceanvim.Opts
 vim.g.rustaceanvim = {
 	tools = {
 		enable_clippy = false,
-		hover_actions = {
+		float_win_config = {
 			auto_focus = true,
+			open_split = "vertical",
+			border = "rounded",
 		},
 	},
 	server = {
+		on_attach = function(_, _)
+			vim.keymap.set("n", "<leader>rme", function()
+				vim.cmd.RustLsp("expandMacro")
+			end, { desc = "[R]ust [M]acro [E]xpand" })
+
+			vim.keymap.set("n", "<leader>rmr", function()
+				vim.cmd.RustLsp("rebuildProcMacros")
+			end, { desc = "[R]ust [M]acro [R]ebuild" })
+
+			vim.keymap.set("n", "<leader>e", function()
+				vim.cmd.RustLsp("renderDiagnostic", "current")
+			end, { desc = "hover diagnostic" })
+
+			vim.keymap.set("n", "gtt", function()
+				vim.cmd.RustLsp("relatedTests")
+			end, { desc = "[G]oto related [T]ests" })
+
+			vim.keymap.set("n", "<leader>roc", function()
+				vim.cmd.RustLsp("openCargo")
+			end, { desc = "[R]ust [O]pen [C]argo" })
+
+			vim.keymap.set("n", "<leader>rod", function()
+				vim.cmd.RustLsp("openDocs")
+			end, { desc = "[R]ust [O]pen [D]ocs" })
+
+			vim.keymap.set("n", "<leader>rpm", function()
+				vim.cmd.RustLsp("parentModule")
+			end, { desc = "[R]ust [P]arent [M]odule" })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "qf",
+				callback = function()
+					if vim.fn.getqflist({ title = 0 }).title ~= "related tests" then
+						return
+					end
+					vim.schedule(function()
+						vim.cmd.cclose()
+						require("telescope.builtin").quickfix({ prompt_title = "Related Tests" })
+					end)
+				end,
+			})
+		end,
+
 		default_settings = {
 			-- rust-analyzer language server configuration
 			["rust-analyzer"] = {
